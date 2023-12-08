21072b4fa2f30f8af9da0c8ad43a54d5ee443af6
chetincho <martin.ferraguti@gmail.com>
2023-12-08 15:28:36 -0300

    Update 1001_introduccion.py

--- 1001 Conceptos basicos/1001_introduccion.py
+++ 1001 Conceptos basicos/1001_introduccion.py
@@ -1,12 +1,21 @@
 """
-Comencemos por escribir nuestro primer programa. Posiblemente "Hello world" sea el programa mas escrito por la comunidad de desarrollo, y es que se ha transformado en el puntapie inicial para aprender un nuevo lenguaje.
+Comencemos por escribir nuestro primer programa. "Hello, World" posiblemente sea el programa más escrito por la comunidad de desarrollo, 
+ya que se ha convertido en el puntapié inicial para quienes desean aprender un nuevo lenguaje de programación.
 
-El comando print nos permite mostrar un mensaje en pantalla para ello discho mensaje debe estar encerrado entre comillas dobles o simples
+El comando [print] nos permite mostrar un mensaje en pantalla. Para lograrlo, dicho mensaje debe estar encerrado entre comillas dobles o simples.
 
-A continuacion veeras un ejemplo de ello.
-
+A continuación, podrás observar un ejemplo.
 """
+
 
 print("Hello world!")   # Ejemplo de impresion de un mensaje en pantalla usando comillas dobles
 
 print('Hello world!')   # Mismo ejemplo pero utilizando comillas simples
+
+
+"""
+En lo personal, prefiero usar comillas dobles, pero nunca debe ser una combinación de ambos, ya que esto arrojará un error de sintaxis (SyntaxError).
+Puedes probarlo por ti mismo escribiendo la siguiente línea de código:
+
+print("Hello world!")   # Como puedes observar, la frase está encerrada por comillas dobles al inicio y comillas simples al final
+"""
