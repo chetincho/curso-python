79a9c31e485b72c991b02ccc8e0bb9ffaffef77f
chetincho <martin.ferraguti@gmail.com>
2023-12-08 15:16:00 -0300

    Update 1001_introduccion.py

--- 1001 Conceptos basicos/1001_introduccion.py
+++ 1001 Conceptos basicos/1001_introduccion.py
@@ -1,1 +1,12 @@
-print("Hello world!")
+"""
+Comencemos por escribir nuestro primer programa. Posiblemente "Hello world" sea el programa mas escrito por la comunidad de desarrollo, y es que se ha transformado en el puntapie inicial para aprender un nuevo lenguaje.
+
+El comando print nos permite mostrar un mensaje en pantalla para ello discho mensaje debe estar encerrado entre comillas dobles o simples
+
+A continuacion veeras un ejemplo de ello.
+
+"""
+
+print("Hello world!")   # Ejemplo de impresion de un mensaje en pantalla usando comillas dobles
+
+print('Hello world!')   # Mismo ejemplo pero utilizando comillas simples
