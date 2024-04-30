0d50fc7b67001f81b7d5de2ac9cd52d4096dad8b
chetincho <martin.ferraguti@gmail.com>
2024-04-30 18:57:33 -0300

    300420241857

--- 1000 Conceptos basicos/1100 Introduccion/1104_introduccion.py
+++ 1000 Conceptos basicos/1100 Introduccion/1104_introduccion.py
@@ -9,23 +9,3 @@ print("-"*7 + " "*4  + "-"*7)  #18
 print("-"*5 + " "*8  + "-"*5)  #18
 print("-"*3 + " "*12 + "-"*3)  #18
 print("-"*1 + " "*16 + "-"*1)  #18
-
-print("\033[89mTexto en rojo\033[0m")
-print("\033[90mTexto en rojo\033[0m")
-print("\033[91mTexto en rojo\033[0m")
-print("\033[92mTexto en rojo\033[0m")
-print("\033[93mTexto en rojo\033[0m")
-print("\033[94mTexto en rojo\033[0m")
-print("\033[95mTexto en rojo\033[0m")
-print("\033[96mTexto en rojo\033[0m")
-print("\033[97mTexto en rojo\033[0m")
-print("\033[98mTexto en rojo\033[0m")
-print("\033[99mTexto en rojo\033[0m")
-print("\033[100mTexto en rojo\033[0m")
-print("\033[101mTexto en rojo\033[0m")
-print("\033[102mTexto en rojo\033[0m")
-print("\033[103mTexto en rojo\033[0m")
-print("\033[104mTexto en rojo\033[0m")
-print("\033[105mTexto en rojo\033[0m")
-print("\033[106mTexto en rojo\033[0m")
-print("\033[107mTexto en rojo\033[0m")
